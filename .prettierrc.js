92c8df15796a128e16c700571e8bd9937a7ad400
lumen-limitless <lumenlimitless@gmail.com>
2023-01-15 21:11:21 -0500

    fixed prettier

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  useTabs: false,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: false,
+  overrides: [
+      {
+          files: '*.sol',
+          options: {
+              printWidth: 120,
+              singleQuote: false,
+              explicitTypes: 'always',
+              parser: 'solidity-parse',
+          },
+      },
+  ],
+  plugins: [require('prettier-plugin-solidity')],
+};
